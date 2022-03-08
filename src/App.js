2c565640886f945d01476a9e0290b174a6a2ec9f
Larry Win <lawrence.j.win@gmail.com>
2022-03-07 21:18:22 -0600

    Stream Cologne by beabadoobee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import logo from './assets/convergent-logo.png';
+import demo_day from './assets/demo_day_1.jpeg';
+import kickoff from './assets/kickoff_1.jpeg';
+import officers from './assets/officers2.jpeg';
+
+function Slideshow (props) {
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    setIndex((index + 1) % props.images.length)
+  }
+
+  const handlePrev = () => {
+    setIndex((index - 1) % props.images.length)
+  }
+
+  return (
+    <div className="slideshow">
+      <button className="slideshowButton" onClick={handlePrev}>❮</button>
+      <div className="slideshowImages">
+        { props.images.map((imageUrl, idx) => {
+          return <img style={{ opacity: Math.abs(index) === idx ? 1 : 0 }} src={imageUrl}/>
+        })}
+      </div>
+      <button className="slideshowButton" onClick={handleNext}>❯</button>
+    </div>
+  )
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img className="logo" src={logo} alt="Texas Convergent Logo" />
+      <h1 className="title">Texas <span className="blue">Convergent</span></h1>
+      <div className="subtitle">Enabling innovation at the intersection of technology, business, and design.</div>
+      <div className="linkButtonContainer">
+        <a className="linkButton blue" href="https://www.txconvergent.org/join">Join Convergent</a>
+        <a className="linkButton" href="https://www.txconvergent.org/buildteams">Learn More About Build Teams</a>
+        <a className="linkButton" href="https://www.txconvergent.org/">Learn More About Forge</a>
+      </div>
+      <Slideshow images={[demo_day, kickoff, officers]} />
     </div>
   );
 }
